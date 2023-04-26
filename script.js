225ad74f1e469caa8f3246749678c5cf1573cbef
ct <catoro17@misena.edu.co>
2023-04-25 22:08:49 -0500

    navbar desktop css

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 
-// trabajamos con la libreria de swiper para el slider 1
-// var swiper = new Swiper(".mySwiper-1",{
-//     slidesPerView:1,
-//     spaceBettween: 30,  
-//     loop: true,
-//     pagination:{
-//         el:".swiper-pagination",
-//         clickable:true,
-//     },
-//     navigation:{
-//         nextEl:".swiper-button-next",
-//         prevEl:".swiper-button-prev"
-//     }
-// });
+
 // trabajamos con la libreria de swiper par el slider 
 var swiper = new Swiper(".mySwiper",{
     slidesPerView:5,
